e76b4cee2a14101cf215b6e27627471ee7d7294b
shailender <shailenderg7@gmail.com>
2022-07-04 10:23:18 +0530

    firts commit

---
+++ packageshail/__init__.py
@@ -0,0 +1,6 @@
+class Good:
+    def __init__(self):
+        print("constuctor is working")
+    def goodfunc(self,number):
+        print("this is a testing of function")
+        return number
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(name="packageshail",
+version = "0.4",
+description = "this is code with shailender",
+packages = ['packageshail'],
+install_requires=[])
